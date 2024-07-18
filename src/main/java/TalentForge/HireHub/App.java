9205d69c423ab80e2de72c5597d72b5b3fd8537a
TalentForgeBnu <talentforgeblu@gmail.com>
2024-07-18 20:25:36 -0300

    added project files
    commit with project files

---
+++ src/main/java/TalentForge/HireHub/App.java
@@ -0,0 +1,13 @@
+package TalentForge.HireHub;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
