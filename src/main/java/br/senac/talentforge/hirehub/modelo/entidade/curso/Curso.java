535572611cfa2b7429bd436239c29c3460cc0ac1
Elisa <vitoriaelisa114@gmail.com>
2024-08-07 16:46:45 -0300

    Anotações da Classe Curso

--- src/main/java/br/senac/talentforge/hirehub/modelo/entidade/curso/Curso.java
+++ src/main/java/br/senac/talentforge/hirehub/modelo/entidade/curso/Curso.java
@@ -1,12 +1,39 @@
 package br.senac.talentforge.hirehub.modelo.entidade.curso;
 
+import java.io.Serializable;
 
-public class Curso {
+import br.senac.talentforge.hirehub.modelo.entidade.professor.Professor;
+import br.senac.talentforge.hirehub.modelo.entidade.statuscurso.StatusCurso;
+
+@Entity
+@Table(name = "curso" )
+public class Curso implements Serializable {
 	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTIFY)
+	@Column(name = "id_professor")
+	private Long id;
+	
+	@Column(name = "nome_curso", leigth = 50, nullable = false, unique = true)
 	private String nomeCurso;
+	
+	@Colum(name = "area_atuacao", leigth = 50, nullable = false, unique = false)
     private String areaDeAtuacao;
+	
+	@Column(name = "statusCurso")
     private StatusCurso status;
     
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "id_professor")
+	private Professor professor;
+	
+	
+	public Curso() {
+		
+	}
+	
     public Curso(String nomeCurso, String areaDeAtuacao, StatusCurso status) {
     	this.nomeCurso = nomeCurso;
     	this.areaDeAtuacao = areaDeAtuacao;
@@ -37,6 +64,15 @@ public class Curso {
     public void setStatus(StatusCurso status) {
         this.status = status;
     }
+    
+    public Professor getProfessor() {
+    	return professor;
+    }
+    
+    public void setProfessor(Professor professor) {
+    	this.professor = professor;
+    }
+    
     
 	
 }
