690ff3ce43929d23ddd8525211010167dc33e3c3
Thiago Vinicius Formento <thiagovfbr2@gmail.com>
2024-08-09 15:21:25 -0300

    Atualizaçao da Classe Aluno

--- src/main/java/br/senac/talentforge/hirehub/modelo/entidade/aluno/Aluno.java
+++ src/main/java/br/senac/talentforge/hirehub/modelo/entidade/aluno/Aluno.java
@@ -26,8 +26,8 @@ private static final long serialVersionUID = 1L;
 	@Column(name = "dossie", length = 200, nullable = false, unique = false)
 	private String dossie;
 	
-	@Column(name = "status_aluno","CURSANDO","FORMADO","DESISTENTE" , nullable = false, unique = false)
-	private StatusAluno status;
+	@Column(name = "AlunoMatriculado","CURSANDO","FORMADO","DESISTENTE" , nullable = false, unique = false)
+	private AlunoMatriculado status;
 	
 	public Aluno() {
 		
