a6ca340441df22f8c3eaa81cde6d8fa30de51252
Thiago Vinicius Formento <thiagovfbr2@gmail.com>
2024-08-12 14:03:15 -0300

    Anotação da Classe Empresa

--- src/main/java/br/senac/talentforge/hirehub/modelo/entidade/empresa/Empresa.java
+++ src/main/java/br/senac/talentforge/hirehub/modelo/entidade/empresa/Empresa.java
@@ -1,21 +1,42 @@
 package TalentForge.HireHub;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Empresa extends Usuario {
+import javax.persistence.CascadeType;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
+
+
+@Entity
+@Table(name = "empresa")
+public class Empresa extends Usuario implements Serializable {
+	
+	
+private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_empresa")
+	private long id;
+	
+	@Column(name = "nomeEmpresa", length = 25, nullable = false, unique = false)
 	private String nomeEmpresa;
+	
+	@Column(name = "cnpj", length = 14, nullable = false, unique = false)
 	private String cnpj;
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "empresa",cascade = CascadeType.ALL, orphanRemoval = true)
 	private List<Vaga> demandaVagas;
-	
-	
 	
 	public Empresa(String nomeEmpresa, String cnpj, List<Vaga> vaga) {
 		super();
 		setNomeEmpresa(nomeEmpresa);
 		setCnpj(cnpj);
 		this.demandaVagas = new ArrayList<>();
-		 }
+		setId(id);
+	}
 
 
 
@@ -57,5 +78,12 @@ public class Empresa extends Usuario {
 
 	}
 	
+	public long getId() {
+		return id;		
+	}
+	
+	public void setId(long id) {
+		this.id = id;
+	}
 
 }
