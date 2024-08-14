91d3aa064a359947c4b2a7348f862c9acc4e30b8
ERICK VINICIUS DOS SANTOS LABER <erick.laber@alunos.sc.senac.br>
2024-08-14 14:39:30 -0300

    anotação da classe oferta

--- src/main/java/br/senac/talentforge/hirehub/modelo/entidade/oferta/Oferta.java
+++ src/main/java/br/senac/talentforge/hirehub/modelo/entidade/oferta/Oferta.java
@@ -1,15 +1,31 @@
 package br.senac.talentforge.hirehub.modelo.entidade.oferta;
 
+import java.io.ObjectInputFilter.Status;
+
+import javax.persistence.Column;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
 import br.senac.talentforge.hirehub.modelo.entidade.curso.Curso;
 import br.senac.talentforge.hirehub.modelo.entidade.vaga.Vaga;
 
 public class Oferta {
 
-	private Vaga vaga;
-	private Curso curso;
+	@Column(name = "proposta", length  = 50, unique = false)
 	private String proposta;
+	@Column(name = "resposta", length  = 50, unique = false)
 	private String resposta;
-	//private Status andamentoOferta;
+	@Enumerated(EnumType.STRING)
+	private Status andamentoOferta;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "codigo_vaga")
+	private Vaga vaga;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "curso")
+	private Curso curso;
 
 	public Vaga getVaga() {
 		return vaga;
