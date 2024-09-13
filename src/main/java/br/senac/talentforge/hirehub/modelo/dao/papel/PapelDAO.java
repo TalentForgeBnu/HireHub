6ca131d8f59067ca6e14461ad9c30067a6f45f03
TalentForge <talentforgeblu@gmail.com>
2024-09-13 16:45:25 -0300

    TRF07-13 FEITO O DAO E ENTIDADE PAPEL

---
+++ src/main/java/br/senac/talentforge/hirehub/modelo/dao/papel/PapelDAO.java
@@ -0,0 +1,13 @@
+package br.senac.talentforge.hirehub.modelo.dao.papel;
+
+import br.senac.talentforge.hirehub.modelo.entidade.papel.Papel;
+
+public interface PapelDAO {
+
+	
+	void inserirPapel(Papel papel);
+	
+	void deletarPapel(Papel papel);
+		
+	void atualizarPapel(Papel papel);
+}
---
+++ src/main/java/br/senac/talentforge/hirehub/modelo/entidade/papel/Papel.java
@@ -0,0 +1,55 @@
+package br.senac.talentforge.hirehub.modelo.entidade.papel;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
+
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+@Table(name = "papel")
+public class Papel implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_papel")
+    private long id;
+	
+	@Column(name = "nome_papel", length = 20, nullable = false, unique = false)
+    private String nomePapel;
+	
+	
+	public Papel() {
+
+    }
+	
+	public Papel(long id, String nomePapel) {
+		setId(id);
+		setNomePapel(nomePapel);
+	}
+	
+	public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+    
+    public void setNomePapel(String nomePapel) {
+    	this.nomePapel = nomePapel;
+    }
+    
+    public String getNomePapel() {
+    	return nomePapel;
+    }
+}
