7e3f487bdb6b7231748f051d30b75fc3e0088632
Jonathan Alfredo da Silva <89150357+Jonathan-A-Silva@users.noreply.github.com>
2024-09-30 13:56:23 -0300

    Update Empresa.java

--- src/main/java/br/senac/talentforge/hirehub/modelo/entidade/empresa/Empresa.java
+++ src/main/java/br/senac/talentforge/hirehub/modelo/entidade/empresa/Empresa.java
@@ -68,15 +68,15 @@ public class Empresa extends PessoaJuridica implements Serializable {
         this.vagas.add(vaga);
     }
 
-    public List<Proposta> getOfertas() {
+    public List<Proposta> getProposta() {
         return propostas;
     }
 
-    public void setOfertas(List<Proposta> propostas) {
+    public void setProposta(List<Proposta> propostas) {
         this.propostas = propostas;
     }
 
-    public void adicionarOferta(Proposta propostas) {
+    public void adicionarProposta(Proposta propostas) {
         this.propostas.add(propostas);
     }
 
