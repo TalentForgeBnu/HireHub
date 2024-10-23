c456480524edaf66ed4fca825d376bd63f992f04
JOAO PEDRO MENDES ATAIDE <joao.ataide@alunos.sc.senac.br>
2024-10-23 15:00:36 -0300

    atualizado vagaDAO para servelet de vaga

--- src/main/java/br/senac/talentforge/hirehub/modelo/dao/vaga/VagaDAO.java
+++ src/main/java/br/senac/talentforge/hirehub/modelo/dao/vaga/VagaDAO.java
@@ -1,19 +1,23 @@
 package br.senac.talentforge.hirehub.modelo.dao.vaga;
 
+import java.util.List;
+
 import br.senac.talentforge.hirehub.modelo.entidade.vaga.Vaga;
 import br.senac.talentforge.hirehub.modelo.enumeracao.contratacao.Contratacao;
 import br.senac.talentforge.hirehub.modelo.enumeracao.situacao.Situacao;
 
 public interface VagaDAO {
-    void inserirVaga(Vaga vaga);
+	void inserirVaga(Vaga vaga);
 
-    void deletarVaga(Vaga vaga);
+	void deletarVaga(Vaga vaga);
 
-    void atualizarVaga(Vaga vaga);
+	void atualizarVaga(Vaga vaga);
 
-    Vaga recuperarVagaPeloIdEmpresa(long codigo);
+	Vaga recuperarVagaPeloIdEmpresa(long codigo);
 
-    Vaga recuperarVagaPelaModalidade(Contratacao modalidadeContratacao);
+	Vaga recuperarVagaPelaModalidade(Contratacao modalidadeContratacao);
 
-    Vaga recuperarVagaPelaSituacaoVaga(Situacao situacaoVaga);
+	Vaga recuperarVagaPelaSituacaoVaga(Situacao situacaoVaga);
+
+	List<Vaga> recuperarVagasPeloIdDaEmpresa(long idEmpresa);
 }
--- src/main/java/br/senac/talentforge/hirehub/modelo/dao/vaga/VagaDAOImpl.java
+++ src/main/java/br/senac/talentforge/hirehub/modelo/dao/vaga/VagaDAOImpl.java
@@ -1,5 +1,7 @@
 package br.senac.talentforge.hirehub.modelo.dao.vaga;
 
+import java.util.List;
+
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
@@ -14,124 +16,144 @@ import br.senac.talentforge.hirehub.modelo.factory.conexao.ConexaoFactory;
 
 public class VagaDAOImpl implements VagaDAO {
 
-    private ConexaoFactory fabrica;
+	private ConexaoFactory fabrica;
 
-    public VagaDAOImpl() {
-        fabrica = new ConexaoFactory();
-    }
+	public VagaDAOImpl() {
+		fabrica = new ConexaoFactory();
+	}
 
-    public void inserirVaga(Vaga vaga) {
-        Session sessao = null;
-        try {
-            sessao = fabrica.getConexao().openSession();
-            sessao.beginTransaction();
-            sessao.save(vaga);
-            sessao.getTransaction().commit();
-        } catch (Exception exception) {
-            erroSessao(sessao, exception);
-        } finally {
-            fecharSessao(sessao);
-        }
-    }
+	public void inserirVaga(Vaga vaga) {
+		Session sessao = null;
+		try {
+			sessao = fabrica.getConexao().openSession();
+			sessao.beginTransaction();
+			sessao.save(vaga);
+			sessao.getTransaction().commit();
+		} catch (Exception exception) {
+			erroSessao(sessao, exception);
+		} finally {
+			fecharSessao(sessao);
+		}
+	}
 
-    public void deletarVaga(Vaga vaga) {
-        Session sessao = null;
-        try {
-            sessao = fabrica.getConexao().openSession();
-            sessao.beginTransaction();
-            sessao.delete(vaga);
-            sessao.getTransaction().commit();
-        } catch (Exception exception) {
-            erroSessao(sessao, exception);
-        } finally {
-            fecharSessao(sessao);
-        }
-    }
+	public void deletarVaga(Vaga vaga) {
+		Session sessao = null;
+		try {
+			sessao = fabrica.getConexao().openSession();
+			sessao.beginTransaction();
+			sessao.delete(vaga);
+			sessao.getTransaction().commit();
+		} catch (Exception exception) {
+			erroSessao(sessao, exception);
+		} finally {
+			fecharSessao(sessao);
+		}
+	}
 
-    public void atualizarVaga(Vaga vaga) {
-        Session sessao = null;
-        try {
-            sessao = fabrica.getConexao().openSession();
-            sessao.beginTransaction();
-            sessao.update(vaga);
-            sessao.getTransaction().commit();
-        } catch (Exception exception) {
-            erroSessao(sessao, exception);
-        } finally {
-            fecharSessao(sessao);
-        }
-    }
+	public void atualizarVaga(Vaga vaga) {
+		Session sessao = null;
+		try {
+			sessao = fabrica.getConexao().openSession();
+			sessao.beginTransaction();
+			sessao.update(vaga);
+			sessao.getTransaction().commit();
+		} catch (Exception exception) {
+			erroSessao(sessao, exception);
+		} finally {
+			fecharSessao(sessao);
+		}
+	}
 
-    public Vaga recuperarVagaPeloIdEmpresa(long codigo) {
-        Session sessao = null;
-        Vaga vagaRecuperada = null;
-        try {
-            sessao = fabrica.getConexao().openSession();
-            sessao.beginTransaction();
-            CriteriaBuilder construtor = sessao.getCriteriaBuilder();
-            CriteriaQuery<Vaga> criteria = construtor.createQuery(Vaga.class);
-            Root<Vaga> raizVaga = criteria.from(Vaga.class);
-            criteria.select(raizVaga).where(construtor.equal(raizVaga.get(Vaga_.EMPRESA), codigo));
-            vagaRecuperada = sessao.createQuery(criteria).getSingleResult();
-            sessao.getTransaction().commit();
-        } catch (Exception exception) {
-            erroSessao(sessao, exception);
-        } finally {
-            fecharSessao(sessao);
-        }
-        return vagaRecuperada;
-    }
+	public Vaga recuperarVagaPeloIdEmpresa(long codigo) {
+		Session sessao = null;
+		Vaga vagaRecuperada = null;
+		try {
+			sessao = fabrica.getConexao().openSession();
+			sessao.beginTransaction();
+			CriteriaBuilder construtor = sessao.getCriteriaBuilder();
+			CriteriaQuery<Vaga> criteria = construtor.createQuery(Vaga.class);
+			Root<Vaga> raizVaga = criteria.from(Vaga.class);
+			criteria.select(raizVaga).where(construtor.equal(raizVaga.get(Vaga_.EMPRESA), codigo));
+			vagaRecuperada = sessao.createQuery(criteria).getSingleResult();
+			sessao.getTransaction().commit();
+		} catch (Exception exception) {
+			erroSessao(sessao, exception);
+		} finally {
+			fecharSessao(sessao);
+		}
+		return vagaRecuperada;
+	}
 
-    public Vaga recuperarVagaPelaModalidade(Contratacao modalidadeContratacao) {
-        Session sessao = null;
-        Vaga vagaRecuperada = null;
-        try {
-            sessao = fabrica.getConexao().openSession();
-            sessao.beginTransaction();
-            CriteriaBuilder construtor = sessao.getCriteriaBuilder();
-            CriteriaQuery<Vaga> criteria = construtor.createQuery(Vaga.class);
-            Root<Vaga> raizVaga = criteria.from(Vaga.class);
-            criteria.select(raizVaga).where(construtor.equal(raizVaga.get(Vaga_.contratacao), modalidadeContratacao));
-            vagaRecuperada = sessao.createQuery(criteria).getSingleResult();
-            sessao.getTransaction().commit();
-        } catch (Exception exception) {
-            erroSessao(sessao, exception);
-        } finally {
-            fecharSessao(sessao);
-        }
-        return vagaRecuperada;
-    }
+	public Vaga recuperarVagaPelaModalidade(Contratacao modalidadeContratacao) {
+		Session sessao = null;
+		Vaga vagaRecuperada = null;
+		try {
+			sessao = fabrica.getConexao().openSession();
+			sessao.beginTransaction();
+			CriteriaBuilder construtor = sessao.getCriteriaBuilder();
+			CriteriaQuery<Vaga> criteria = construtor.createQuery(Vaga.class);
+			Root<Vaga> raizVaga = criteria.from(Vaga.class);
+			criteria.select(raizVaga).where(construtor.equal(raizVaga.get(Vaga_.contratacao), modalidadeContratacao));
+			vagaRecuperada = sessao.createQuery(criteria).getSingleResult();
+			sessao.getTransaction().commit();
+		} catch (Exception exception) {
+			erroSessao(sessao, exception);
+		} finally {
+			fecharSessao(sessao);
+		}
+		return vagaRecuperada;
+	}
 
-    public Vaga recuperarVagaPelaSituacaoVaga(Situacao situacaoVaga) {
-        Session sessao = null;
-        Vaga vagaRecuperada = null;
-        try {
-            sessao = fabrica.getConexao().openSession();
-            sessao.beginTransaction();
-            CriteriaBuilder construtor = sessao.getCriteriaBuilder();
-            CriteriaQuery<Vaga> criteria = construtor.createQuery(Vaga.class);
-            Root<Vaga> raizVaga = criteria.from(Vaga.class);
-            criteria.select(raizVaga).where(construtor.equal(raizVaga.get(Vaga_.SITUACAO), situacaoVaga));
-            vagaRecuperada = sessao.createQuery(criteria).getSingleResult();
-            sessao.getTransaction().commit();
-        } catch (Exception exception) {
-            erroSessao(sessao, exception);
-        } finally {
-            fecharSessao(sessao);
-        }
-        return vagaRecuperada;
-    }
+	public Vaga recuperarVagaPelaSituacaoVaga(Situacao situacaoVaga) {
+		Session sessao = null;
+		Vaga vagaRecuperada = null;
+		try {
+			sessao = fabrica.getConexao().openSession();
+			sessao.beginTransaction();
+			CriteriaBuilder construtor = sessao.getCriteriaBuilder();
+			CriteriaQuery<Vaga> criteria = construtor.createQuery(Vaga.class);
+			Root<Vaga> raizVaga = criteria.from(Vaga.class);
+			criteria.select(raizVaga).where(construtor.equal(raizVaga.get(Vaga_.SITUACAO), situacaoVaga));
+			vagaRecuperada = sessao.createQuery(criteria).getSingleResult();
+			sessao.getTransaction().commit();
+		} catch (Exception exception) {
+			erroSessao(sessao, exception);
+		} finally {
+			fecharSessao(sessao);
+		}
+		return vagaRecuperada;
+	}
 
-    private void erroSessao(Session sessao, Exception exception) {
-        exception.printStackTrace();
-        if (sessao.getTransaction() != null) {
-            sessao.getTransaction().rollback();
-        }
-    }
+	public List<Vaga> recuperarVagasPeloIdDaEmpresa(long idEmpresa) {
+		Session sessao = null;
+		List<Vaga> vagasRecuperadas = null;
+		try {
+			sessao = fabrica.getConexao().openSession();
+			sessao.beginTransaction();
+			CriteriaBuilder construtor = sessao.getCriteriaBuilder();
+			CriteriaQuery<Vaga> criteria = construtor.createQuery(Vaga.class);
+			Root<Vaga> raizVaga = criteria.from(Vaga.class);
+			criteria.where(construtor.equal(raizVaga.get(Vaga_.EMPRESA), idEmpresa));
+			vagasRecuperadas = sessao.createQuery(criteria).getResultList();
+			sessao.getTransaction().commit();
+		} catch (Exception exception) {
+			erroSessao(sessao, exception);
+		} finally {
+			fecharSessao(sessao);
+		}
+		return vagasRecuperadas;
+	}
 
-    private void fecharSessao(Session sessao) {
-        if (sessao != null) {
-            sessao.close();
-        }
-    }
+	private void erroSessao(Session sessao, Exception exception) {
+		exception.printStackTrace();
+		if (sessao.getTransaction() != null) {
+			sessao.getTransaction().rollback();
+		}
+	}
+
+	private void fecharSessao(Session sessao) {
+		if (sessao != null) {
+			sessao.close();
+		}
+	}
 }
