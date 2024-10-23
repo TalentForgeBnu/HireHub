e3fa11834e251b31b5c127c7778559173796215e
JOAO PEDRO MENDES ATAIDE <joao.ataide@alunos.sc.senac.br>
2024-10-23 14:43:11 -0300

    TRF-4.43 - Atualizado servlet de vaga para usar o usuario logado

--- src/main/java/br/senac/talentforge/hirehub/controle/servlet/VagaServlet.java
+++ src/main/java/br/senac/talentforge/hirehub/controle/servlet/VagaServlet.java
@@ -1,17 +1,17 @@
 package br.senac.talentforge.hirehub.controle.servlet;
-
+ 
 import java.io.IOException;
 import java.sql.SQLException;
-import java.time.LocalDate;
 import java.util.List;
-
+ 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import javax.servlet.http.HttpSession;
+ 
 import br.senac.talentforge.hirehub.modelo.dao.empresa.EmpresaDAO;
 import br.senac.talentforge.hirehub.modelo.dao.empresa.EmpresaDAOImpl;
 import br.senac.talentforge.hirehub.modelo.dao.endereco.EnderecoDAO;
@@ -23,121 +23,131 @@ import br.senac.talentforge.hirehub.modelo.dao.usuario.UsuarioDAOImpl;
 import br.senac.talentforge.hirehub.modelo.dao.vaga.VagaDAO;
 import br.senac.talentforge.hirehub.modelo.dao.vaga.VagaDAOImpl;
 import br.senac.talentforge.hirehub.modelo.entidade.empresa.Empresa;
-import br.senac.talentforge.hirehub.modelo.entidade.endereco.Endereco;
-import br.senac.talentforge.hirehub.modelo.entidade.papel.Papel;
 import br.senac.talentforge.hirehub.modelo.entidade.vaga.Vaga;
 import br.senac.talentforge.hirehub.modelo.enumeracao.contratacao.Contratacao;
 import br.senac.talentforge.hirehub.modelo.enumeracao.situacao.Situacao;
-
-@WebServlet(urlPatterns = {"/inserir-vaga", "/atualizar-vaga", "/recuperar-lista-vagas"})
-public class VagaServlet extends HttpServlet {
-
-    private static final long serialVersionUID = 3963892615335298562L;
-    private VagaDAO vagaDAO;
-    private EmpresaDAO empresaDAO;
-    private EnderecoDAO enderecoDAO;
-    private UsuarioDAO usuarioDAO;
-    private PapelDAO papelDAO;
-
-    public void init() {
-        vagaDAO = new VagaDAOImpl();
-        empresaDAO = new EmpresaDAOImpl();
-        enderecoDAO = new EnderecoDAOImpl();
-        usuarioDAO = new UsuarioDAOImpl();
-        papelDAO = new PapelDAOImpl();
-    }
-
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String action = request.getServletPath();
-        try {
-            switch (action) {
-                case "/inserir-vaga" -> inserirVaga(request, response);
-                case "/recuperar-lista-vagas" -> recuperaListaVagas(request, response);
-                case "/recuperar-vagas" -> recuperarVaga(request, response);
-            }
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        doGet(request, response);
-    }
-
-    private void inserirVaga(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException {
-
-        String estado = "SC";
-        String cidade = "Gaspar";
-        String logradouro = "Alfredo ramos";
-        String bairro = "lala";
-        String cep = "123123";
-        int numero = 123;
-        String complemento = "Casa";
-        String via = "rua";
-        String data = "2024-03-01";
-        LocalDate dataFundacao = LocalDate.parse(data);
-
-        Endereco end = new Endereco(logradouro, bairro, cidade, estado, cep, numero, complemento, via);
-
-        Papel papel = new Papel();
-        papel.setPapel("Sim");
-        papelDAO.inserirPapel(papel);
-
-        enderecoDAO.inserirEndereco(end);
-
-        Empresa empresa = new Empresa("123", end, papel, "4578916475", "empresa@empresa.com", "106572194377", "Empresa", dataFundacao, "sim");
-        usuarioDAO.inserirUsuario(empresa);
-
-        String codigo = request.getParameter("codigo-vaga");
-        String nome = request.getParameter("nome");
-        String descricao = request.getParameter("descricao-vaga");
-        Contratacao contratacao = Contratacao.valueOf(request.getParameter("modalidade-contratacao").toUpperCase());
-        Situacao situacao = Situacao.ABERTA;
-        Vaga vaga = new Vaga(codigo, nome, descricao, contratacao, situacao, empresa);
-        vagaDAO.inserirVaga(vaga);
-    }
-
-    private void recuperaListaVagas(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String estado = "SC";
-        String cidade = "Gaspar";
-        String logradouro = "Alfredo ramos";
-        String bairro = "lala";
-        String cep = "123123";
-        int numero = 123;
-        String complemento = "Casa";
-        String via = "rua";
-        String data = "2024-03-01";
-        LocalDate dataFundacao = LocalDate.parse(data);
-        Endereco end = new Endereco(logradouro, bairro, cidade, estado, cep, numero, complemento, via);
-
-        Papel papel = new Papel("Sim");
-
-        Empresa empresa = new Empresa("123", end, papel, "4578916475", "empresa@empresa.com", "106572194377", "Empresa", dataFundacao, "sim");
-
-        Vaga vaga1 = new Vaga("codigo1", "nome1", "descrição1", Contratacao.MEIO_PERIODO, Situacao.ABERTA, empresa);
-        Vaga vaga2 = new Vaga("codigo2", "nome2", "descrição2", Contratacao.ESTAGIO, Situacao.FECHADA, empresa);
-        Vaga vaga3 = new Vaga("codigo3", "nome3", "descrição3", Contratacao.INTEGRAL, Situacao.ABERTA, empresa);
-        Vaga vaga4 = new Vaga("codigo4", "nome4", "descrição4", Contratacao.MEIO_PERIODO, Situacao.FECHADA, empresa);
-        Vaga vaga5 = new Vaga("codigo5", "nome5", "descrição5", Contratacao.INTEGRAL, Situacao.ABERTA, empresa);
-
-        List<Vaga> vagas = List.of(vaga1, vaga2, vaga3, vaga4, vaga5);
-
-        request.setAttribute("vagas", vagas);
-        RequestDispatcher dispatcher = request.getRequestDispatcher("Paginas/listagem-vagas-empresa.jsp");
-        dispatcher.forward(request, response);
-    }
-    
-    private void recuperarVaga(HttpServletRequest request, HttpServletResponse response)throws IOException, ServletException {
-    	Papel papel = new Papel();
-    	papel.setPapel("sim");
-    	Endereco endereco = new Endereco("rua tal", "um bairro ae", "cidade", "um Estado", "cep", 123, "complemento ai", "via");
-    	Empresa empresa = new Empresa("123",endereco,papel,"123456789","a@a.com","1234567","nomeempresa",LocalDate.now(),"Empresa");
-    	Vaga vaga =  new Vaga("codigo1","nome1","descricao1", Contratacao.MEIO_PERIODO , Situacao.ABERTA, empresa);
-    			
-    	request.setAttribute("vaga", vaga);
-        RequestDispatcher dispatcher = request.getRequestDispatcher("Paginas/listagem-vagas-aluno.jsp");
-        dispatcher.forward(request, response);
-    }
  
-
+@WebServlet(urlPatterns = { "/inserir-vaga", "/atualizar-vaga", "/recuperar-lista-vagas" })
+public class VagaServlet extends HttpServlet {
+ 
+	private static final long serialVersionUID = 3963892615335298562L;
+	private VagaDAO vagaDAO;
+	private EmpresaDAO empresaDAO;
+	private EnderecoDAO enderecoDAO;
+	private UsuarioDAO usuarioDAO;
+	private PapelDAO papelDAO;
+ 
+	public void init() {
+		vagaDAO = new VagaDAOImpl();
+		empresaDAO = new EmpresaDAOImpl();
+		enderecoDAO = new EnderecoDAOImpl();
+		usuarioDAO = new UsuarioDAOImpl();
+		papelDAO = new PapelDAOImpl();
+	}
+ 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String action = request.getServletPath();
+		try {
+			switch (action) {
+			case "/inserir-vaga" -> inserirVaga(request, response);
+			case "/recuperar-lista-vagas" -> recuperaListaVagas(request, response);
+			case "/recuperar-vagas" -> recuperarVaga(request, response);
+			}
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+ 
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+ 
+	private void inserirVaga(HttpServletRequest request, HttpServletResponse response)
+			throws SQLException, IOException {
+ 
+		HttpSession session = request.getSession();
+		Empresa empresa = null;
+ 
+		if (session == null) {
+			response.sendRedirect(request.getContextPath() + ("Paginas/tela-login.jsp"));
+		}
+ 
+		empresa = (Empresa) session.getAttribute("usuario-logado");
+ 
+		if (empresa.equals(session.getAttribute("usuario-logado"))) {
+ 
+			if (empresa != null) {
+ 
+				String codigo = request.getParameter("codigo-vaga");
+				String nome = request.getParameter("nome");
+				String descricao = request.getParameter("descricao-vaga");
+				Contratacao contratacao = Contratacao
+						.valueOf(request.getParameter("modalidade-contratacao").toUpperCase());
+				Situacao situacao = Situacao.ABERTA;
+				Vaga vaga = new Vaga(codigo, nome, descricao, contratacao, situacao, empresa);
+				vagaDAO.inserirVaga(vaga);
+			}
+		}
+	}
+ 
+	private void recuperaListaVagas(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+ 
+		HttpSession session = request.getSession();
+		Empresa empresa = null;
+ 
+		if (session == null) {
+			response.sendRedirect(request.getContextPath() + ("Paginas/tela-login.jsp"));
+		}
+ 
+		empresa = (Empresa) session.getAttribute("usuario-logado");
+ 
+		if (empresa.equals(session.getAttribute("usuario-logado"))) {
+ 
+			if (empresa != null) {
+ 
+				Vaga vaga1 = new Vaga("codigo1", "nome1", "descrição1", Contratacao.MEIO_PERIODO, Situacao.ABERTA,
+						empresa);
+				Vaga vaga2 = new Vaga("codigo2", "nome2", "descrição2", Contratacao.ESTAGIO, Situacao.FECHADA, empresa);
+				Vaga vaga3 = new Vaga("codigo3", "nome3", "descrição3", Contratacao.INTEGRAL, Situacao.ABERTA, empresa);
+				Vaga vaga4 = new Vaga("codigo4", "nome4", "descrição4", Contratacao.MEIO_PERIODO, Situacao.FECHADA,
+						empresa);
+				Vaga vaga5 = new Vaga("codigo5", "nome5", "descrição5", Contratacao.INTEGRAL, Situacao.ABERTA, empresa);
+ 
+				List<Vaga> vagas = List.of(vaga1, vaga2, vaga3, vaga4, vaga5);
+ 
+				request.setAttribute("vagas", vagas);
+				RequestDispatcher dispatcher = request.getRequestDispatcher("Paginas/listagem-vagas-empresa.jsp");
+				dispatcher.forward(request, response);
+			}
+		}
+	}
+ 
+	private void recuperarVaga(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+ 
+		HttpSession session = request.getSession();
+		Empresa empresa = null;
+ 
+		if (session == null) {
+			response.sendRedirect(request.getContextPath() + ("Paginas/tela-login.jsp"));
+		}
+ 
+		empresa = (Empresa) session.getAttribute("usuario-logado");
+ 
+		if (empresa.equals(session.getAttribute("usuario-logado"))) {
+ 
+			if (empresa != null) {
+ 
+				Vaga vaga = new Vaga("codigo1", "nome1", "descricao1", Contratacao.MEIO_PERIODO, Situacao.ABERTA,
+						empresa);
+ 
+				request.setAttribute("vaga", vaga);
+				RequestDispatcher dispatcher = request.getRequestDispatcher("Paginas/listagem-vagas-aluno.jsp");
+				dispatcher.forward(request, response);
+			}
+		}
+	}
 }
