49bf9e47c87049d5e61002c8bc6228c5082cacd6
João Pedro Mendes Ataide <jmendesataide@gmail.com>
2024-11-01 23:10:30 -0300

    TRF-4.52 - implementado método de recuperar uma vaga no servlet de vaga

--- src/main/java/br/senac/talentforge/hirehub/controle/servlet/VagaServlet.java
+++ src/main/java/br/senac/talentforge/hirehub/controle/servlet/VagaServlet.java
@@ -2,6 +2,7 @@ package br.senac.talentforge.hirehub.controle.servlet;
 
 import java.io.IOException;
 import java.util.List;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,6 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
 import br.senac.talentforge.hirehub.modelo.dao.vaga.VagaDAO;
 import br.senac.talentforge.hirehub.modelo.dao.vaga.VagaDAOImpl;
 import br.senac.talentforge.hirehub.modelo.entidade.empresa.Empresa;
@@ -17,95 +19,117 @@ import br.senac.talentforge.hirehub.modelo.entidade.vaga.Vaga;
 import br.senac.talentforge.hirehub.modelo.enumeracao.contratacao.Contratacao;
 import br.senac.talentforge.hirehub.modelo.enumeracao.situacao.Situacao;
 
-@WebServlet(urlPatterns = {"/inserir-vaga", "/recuperar-lista-vaga"})
+@WebServlet(urlPatterns = { "/inserir-vaga", "/recuperar-lista-vaga", "/recuperar-vaga" })
 public class VagaServlet extends HttpServlet {
 
-    private static final long serialVersionUID = 3963892615335298562L;
-    private VagaDAO vagaDAO;
+	private static final long serialVersionUID = 3963892615335298562L;
+	private VagaDAO vagaDAO;
 
-    public void init() {
-        vagaDAO = new VagaDAOImpl();
-    }
+	public void init() {
+		vagaDAO = new VagaDAOImpl();
+	}
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        String action = request.getServletPath();
-        try {
-            switch (action) {
-                case "/inserir-vaga" -> inserirVaga(request, response);
-                case "/recuperar-lista-vaga" -> recuperarListaVagas(request, response);
-            }
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String action = request.getServletPath();
+		try {
+			switch (action) {
+			case "/inserir-vaga" -> inserirVaga(request, response);
+			case "/recuperar-lista-vaga" -> recuperarListaVagas(request, response);
+			case "/recuperar-vaga" -> recuperarVaga(request, response);
+			}
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        doGet(request, response);
-    }
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
 
-    private void inserirVaga(HttpServletRequest request, HttpServletResponse response)
-            throws IOException {
+	private void inserirVaga(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-        HttpSession session = request.getSession();
-        Usuario usuario = null;
-        Empresa empresa = null;
+		HttpSession session = request.getSession();
+		Usuario usuario = null;
+		Empresa empresa = null;
 
-        if (session == null && session.getAttribute("usuario-logado") == null) {
-            response.sendRedirect(request.getContextPath() + "Paginas/tela-login.jsp");
-        }
+		if (session == null || session.getAttribute("usuario-logado") == null) {
+			response.sendRedirect(request.getContextPath() + "Paginas/tela-login.jsp");
+		}
 
-        usuario = (Usuario) session.getAttribute("usuario-logado");
+		usuario = (Usuario) session.getAttribute("usuario-logado");
 
-        if (usuario.getPapel().getFuncao().equals("empresa")) {
+		if (usuario.getPapel().getFuncao().equals("empresa")) {
 
-            empresa = (Empresa) usuario;
+			empresa = (Empresa) usuario;
 
-            String codigo = request.getParameter("codigo-vaga");
-            String nome = request.getParameter("nome");
-            String descricao = request.getParameter("descricao-vaga");
-            Contratacao contratacao = Contratacao.valueOf(request.getParameter("modalidade-contratacao").toUpperCase());
-            Situacao situacao = Situacao.ABERTA;
+			String codigo = request.getParameter("codigo-vaga");
+			String nome = request.getParameter("nome");
+			String descricao = request.getParameter("descricao-vaga");
+			Contratacao contratacao = Contratacao.valueOf(request.getParameter("modalidade-contratacao").toUpperCase());
+			Situacao situacao = Situacao.ABERTA;
 
-            Vaga vaga = new Vaga(codigo, nome, descricao, contratacao, situacao, empresa);
+			Vaga vaga = new Vaga(codigo, nome, descricao, contratacao, situacao, empresa);
 
-            vagaDAO.inserirVaga(vaga);
+			vagaDAO.inserirVaga(vaga);
 
-            response.sendRedirect(request.getContextPath());
-        } else {
-            response.sendRedirect(request.getContextPath());
-        }
+			response.sendRedirect(request.getContextPath());
+		} else {
+			response.sendRedirect(request.getContextPath());
+		}
 
-    }
+	}
 
-    private void recuperarListaVagas(HttpServletRequest request, HttpServletResponse response)
-            throws IOException, ServletException {
+	private void recuperarListaVagas(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
 
-        HttpSession session = request.getSession();
-        Usuario usuario = null;
-        Empresa empresa = null;
+		HttpSession session = request.getSession();
+		Usuario usuario = null;
+		Empresa empresa = null;
 
-        if (session == null && session.getAttribute("usuario-logado") == null) {
-            response.sendRedirect(request.getContextPath() + "Paginas/tela-login.jsp");
-        }
+		if (session == null || session.getAttribute("usuario-logado") == null) {
+			response.sendRedirect(request.getContextPath() + "Paginas/tela-login.jsp");
+		}
 
-        usuario = (Usuario) session.getAttribute("usuario-logado");
+		usuario = (Usuario) session.getAttribute("usuario-logado");
 
-        if (usuario.getPapel().getFuncao().equals("empresa")) {
+		if (usuario.getPapel().getFuncao().equals("empresa")) {
 
-            empresa = (Empresa) usuario;
+			empresa = (Empresa) usuario;
 
-            List<Vaga> vagas = vagaDAO.recuperarVagasPeloIdDaEmpresa(empresa.getId());
+			List<Vaga> vagas = vagaDAO.recuperarVagasPeloIdDaEmpresa(empresa.getId());
 
-            request.setAttribute("vagas", vagas);
-            RequestDispatcher dispatcher = request.getRequestDispatcher("Paginas/listagem-vagas-empresa.jsp");
-            dispatcher.forward(request, response);
-        } else {
-            response.sendRedirect(request.getContextPath());
-        }
+			request.setAttribute("vagas", vagas);
+			RequestDispatcher dispatcher = request.getRequestDispatcher("Paginas/listagem-vagas-empresa.jsp");
+			dispatcher.forward(request, response);
+		} else {
+			response.sendRedirect(request.getContextPath());
+		}
+	}
 
-    }
+	private void recuperarVaga(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
 
+		HttpSession session = request.getSession();
+		Vaga vaga = null;
+		Usuario usuario = null;
+
+		if (session == null || session.getAttribute("usuario-logado") == null) {
+			response.sendRedirect(request.getContextPath() + "Paginas/tela-login.jsp");
+		}
+
+		usuario = (Usuario) session.getAttribute("usuario-logado");
+
+		if (usuario.getPapel().getFuncao().equals("empresa")) {
+
+			long idVaga = Long.parseLong(request.getParameter("id-vaga"));
+
+			vaga = vagaDAO.recuperarVagaPeloId(idVaga);
+
+			request.setAttribute("vaga", vaga);
+			RequestDispatcher dispatcher = request.getRequestDispatcher("Paginas/perfil-vaga");
+			dispatcher.forward(request, response);
+		}
+	}
 }
- 
